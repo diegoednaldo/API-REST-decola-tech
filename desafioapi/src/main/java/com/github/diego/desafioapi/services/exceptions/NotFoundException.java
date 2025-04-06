0cc8b941e6b74127be4b6a2bedcaf2e7abca4313
Diego Ednaldo de Santana <diegosantana000@yahoo.com>
2025-04-06 17:01:48 -0300

    Melhorias nos exception

--- desafioapi/src/main/java/com/github/diego/desafioapi/services/exceptions/NotFoundException.java
+++ desafioapi/src/main/java/com/github/diego/desafioapi/services/exceptions/NotFoundException.java
@@ -1,11 +1,7 @@
 package com.github.diego.desafioapi.services.exceptions;
 
 public class NotFoundException extends BusinessException {
-
-    private static final long serialVersionUID = 1L;
-
-    public NotFoundException() {
-        super("Resource not found.");
+    public NotFoundException(String message) {
+        super(message);
     }
-
 }
